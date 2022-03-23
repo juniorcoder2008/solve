07e1874b29416fac7a9e7cb0819149723e12fbc8
JuniorCoder <thomas08samuel@gmail.com>
2022-03-23 17:25:13 +0100

    grammar-fix

--- src/components/Task.svelte
+++ src/components/Task.svelte
@@ -2,15 +2,16 @@
   export let name;
   export let qCount;
   export let uuid;
+
+  $: qCountText =
+    qCount == 1 ? `<span>1</span> Frage` : `<span>${qCount}</span> Fragen`;
 </script>
 
 <div
   class="task xl:w-3/4 md:w-10/12 flex justify-end gap-12 dark:bg-gray-700 dark:text-white py-3 px-5 rounded-md bg-gray-200 relative"
 >
   <h1 class="uppercase font-bold absolute left-5">{name}</h1>
-  <p class="">
-    <span class="font-bold">{qCount}</span> Fragen
-  </p>
+  <p>{qCountText}</p>
   <p class="text-indigo-400 font-medium">
     {uuid}
   </p>
